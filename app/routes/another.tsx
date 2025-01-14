df531f1737f69fb5931ee5269da118b38e0e0354
ekkx <geniusnaru@gmail.com>
2025-01-14 13:56:42 +0900

    feat: add another page

---
+++ app/routes/another.tsx
@@ -0,0 +1,11 @@
+export function meta() {
+  return [{ title: "another" }];
+}
+
+export default function Home() {
+  return (
+    <div className="grid place-items-center h-dvh">
+      <p>another page</p>
+    </div>
+  );
+}
